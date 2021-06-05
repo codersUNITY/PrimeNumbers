ade9f5d8f4547276b3b10fdd8532ba285b1284e0
Balaguru-M <85390971+Balaguru-M@users.noreply.github.com>
2021-06-05 15:22:19 +0530

    Add files via upload

---
+++ primenumbers.py
@@ -0,0 +1,24 @@
+import math
+
+num = int(input("Enter a number: "))
+factors = []
+
+if num > 1:
+    for a in range(1, num+1):
+        quotient = num / a
+        remainder = num % a
+        factor = math.floor(quotient)
+        if remainder == 0:
+            factors.append(factor)
+
+    if len(factors) == 2:
+        print(str(num) + " is a prime number.")
+        print("Proof: ")
+        print("Since " + str(num) + " has only " + str(len(factors)) + " factors " + str(factors) + ", it is a prime number.")
+    else:
+        print(str(num) + " is a composite number.")
+        print("Proof: ")
+        print("Since " + str(num) + " has " + str(len(factors)) + " factors " + str(factors) + ", it is a composite number.")
+else:
+    print(str(num) + " is neither prime nor composite.")
+
